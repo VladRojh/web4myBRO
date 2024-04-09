5bcfb06f857885ee6e23e90aba97ac02a49df6b6
VladRojh <rojvladiv@gmail.com>
2024-04-09 22:59:55 +0300

    finalweb

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 
   const tabItem = document.querySelectorAll(' .tabs__btn-item');
-  const tabcontent = document.querySelectorAll(' .tabs__content-item');
+  const tabContent = document.querySelectorAll(' .tabs__content-item');
 
 
   tabItem.forEach(function(element){
@@ -11,13 +11,30 @@
 
   function open(evt) {
     const tabTarget = evt.currentTarget;
+    const button = tabTarget.dataset.button;
 
     tabItem.forEach(function(iten){
         tabItem.classList.remove('tabs__btn-item--active');
     })
 
     tabTarget.classList.add('tabs__btn-item--active')
-  }
+ 
+
+  tabContent.forEach(function(item){
+    item.classList.remove
+    ('tabs__content-item--active');
+  })
+
+  document.querySelector(`#${button}`).classList.add
+  (`tabs__content-item--active`)
+}
+
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu__list--active')
+})
 
   const swiper = new Swiper(".swiper", {
     effect: "fade",
